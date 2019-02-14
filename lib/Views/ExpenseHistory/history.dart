d18aac9920dff5156939ed39c67067e862a08431
Lwin Maung Phyo <lwinmgphyo95@gmail.com>
2019-02-14 15:48:26 +0630

    Fixed daily history chart data

--- lib/Views/ExpenseHistory/history.dart
+++ lib/Views/ExpenseHistory/history.dart
@@ -25,6 +25,7 @@ class _HistoryState extends State<History> {
   var priceTotal = 0.0;
   var series;
   var _chart = true;
+  List<Expense> dataList = [];
 
   @override
   void initState() {
@@ -47,7 +48,7 @@ class _HistoryState extends State<History> {
           );
           _listExp.add(_expense);
         }
-        _charts(_listExp);
+        _charts();
         setState(() {
           _loading = false;
         });
@@ -73,7 +74,7 @@ class _HistoryState extends State<History> {
           );
           _listExp.add(_expense);
         }
-        _charts(_listExp);
+        _charts();
         setState(() {
           _chart = true;
           _loading = false;
@@ -101,13 +102,71 @@ class _HistoryState extends State<History> {
     });
   }
 
-  _charts(var dailyExpList) {
+  _chartFunction() {
+    dataList.clear();
+    var clothingTotal = 0.0;
+    var entertainmentTotal = 0.0;
+    var foodTotal = 0.0;
+    var giftsTotal = 0.0;
+    var healthTotal = 0.0;
+    var personalTotal = 0.0;
+    var transTotal = 0.0;
+    var utilitiesTotal = 0.0;
+    var clothdata = _listExp.where((cate) => cate.category == "Clothing");
+    var enterdata = _listExp.where((cate) => cate.category == "Entertainment");
+    var fooddata = _listExp.where((cate) => cate.category == "Food");
+    var giftsdata =
+        _listExp.where((cate) => cate.category == "Gifts/Donations");
+    var healthdata =
+        _listExp.where((cate) => cate.category == "Medical/Healthcare");
+    var personaldata = _listExp.where((cate) => cate.category == "Personal");
+    var transdata = _listExp.where((cate) => cate.category == "Transportation");
+    var utitilitiesdata =
+        _listExp.where((cate) => cate.category == "Utilities");
+    for (var item in clothdata) {
+      clothingTotal += item.amount;
+    }
+    for (var item in enterdata) {
+      entertainmentTotal += item.amount;
+    }
+    for (var item in fooddata) {
+      foodTotal += item.amount;
+    }
+    for (var item in giftsdata) {
+      giftsTotal += item.amount;
+    }
+    for (var item in healthdata) {
+      healthTotal += item.amount;
+    }
+    for (var item in personaldata) {
+      personalTotal += item.amount;
+    }
+    for (var item in transdata) {
+      transTotal += item.amount;
+    }
+    for (var item in utitilitiesdata) {
+      utilitiesTotal += item.amount;
+    }
+    dataList = [
+      Expense(category: "Clothing", amount: clothingTotal),
+      Expense(category: "Entertainment", amount: entertainmentTotal),
+      Expense(category: "Food", amount: foodTotal),
+      Expense(category: "Gifts/Donations", amount: giftsTotal),
+      Expense(category: "Medical/Healthcare", amount: healthTotal),
+      Expense(category: "Personal", amount: personalTotal),
+      Expense(category: "Transportation", amount: transTotal),
+      Expense(category: "Utilities", amount: utilitiesTotal),
+    ];
+  }
+
+  _charts() async {
+    await _chartFunction();
     series = [
       new charts.Series<Expense, String>(
           id: 'Sales',
           domainFn: (Expense sales, _) => sales.category,
           measureFn: (Expense sales, _) => sales.amount,
-          data: dailyExpList,
+          data: dataList,
           colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
           labelAccessorFn: (Expense sales, _) =>
               '${sales.category} - \$${sales.amount.toString()}')
