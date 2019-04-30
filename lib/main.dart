67013ecde6527cc5d953a7a817b980cfac3686b3
Lwin Maung Phyo <lwinmgphyo95@gmail.com>
2019-04-30 11:40:05 +0630

    Portrait Mode Only Restriction

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'Views/SplashScreen/splash_screen.dart';
 import 'Views/LandingPage/landing_page.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
         title: "Expenager",
         debugShowCheckedModeBanner: false,
